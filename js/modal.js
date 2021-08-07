e7aa23a4173665dc7cfcf5a801cc6208b68eb22c
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2021-08-07 16:48:02 +0300

    added modal closing on backdrop click

--- js/modal.js
+++ js/modal.js
@@ -7,9 +7,17 @@
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.modal.addEventListener('click', onBackdropClick);
 
   function toggleModal() {
     document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle('is-hidden');
   }
+
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      toggleModal();
+    }
+  }
 })();
+
