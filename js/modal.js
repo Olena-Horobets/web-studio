bf2b265c1b751855175ea06b08da7c5d2c3dd134
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2021-10-08 18:29:37 +0300

    saving commit

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,17 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-  refs.modal.addEventListener('click', onBackdropClick);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.modal.addEventListener("click", onBackdropClick);
 
   function toggleModal() {
     document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle("is-hidden");
   }
 
   function onBackdropClick(event) {
@@ -20,4 +20,3 @@
     }
   }
 })();
-
